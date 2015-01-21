fc26e7eb6a7ed1c0229e6aaaacecb94415b7af6c
Iakov Volf <jakoff@gmail.com>
2015-01-21 13:52:30 +0200

    Initial commit

---
+++ src/test/java/com/insynctive/SampleTest.java
@@ -0,0 +1,25 @@
+package com.insynctive;
+
+import org.openqa.selenium.support.PageFactory;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.insynctive.pages.HomePage;
+
+public class SampleTest extends TestBase {
+
+  private HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
